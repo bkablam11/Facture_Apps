0a4feda9f6f76066b440ef165ce7416fc8c18d78
bkablam11 <bkablam11@gmail.com>
2025-02-15 17:18:28 +0000

    adding prisma, clerk, daily

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -2,6 +2,11 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import {
+  ClerkProvider,
+} from '@clerk/nextjs'
+
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -23,12 +28,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <ClerkProvider>
+    <html lang="en" data-theme=" fantasy ">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }
